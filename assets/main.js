2f67ee01d56e884dfe697a6b7887b8ada3f4818d
rhetesi <rhetesi@gmail.com>
2023-01-27 22:15:11 +0100

    Edit main.js

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@
 
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
 };
 
 const sumTotal = document.querySelector('.sumTotal');
