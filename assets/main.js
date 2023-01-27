32afbeda335ffb03eae3363663fc6135e40561ad
rhetesi <rhetesi@gmail.com>
2023-01-27 22:17:18 +0100

    Edit main.js service worket path

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@
 
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('/change/sw.js');
 };
 
 const sumTotal = document.querySelector('.sumTotal');
